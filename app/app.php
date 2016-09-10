06f5b918bdd360a3f497d5286ba2689933c845ee
Ryan Loos <RLoos289@gmail.com>
2016-09-10 11:51:55 -0700

    add debugger

--- app/app.php
+++ app/app.php
@@ -10,6 +10,8 @@
 
     $app = new Silex\Application();
 
+    $app['debug'] = true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
